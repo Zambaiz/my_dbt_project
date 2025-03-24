7da0caa391039001075c4ed0a475cf294e41f098
Zamabaiz <68869667+Zambaiz@users.noreply.github.com>
2025-03-24 05:49:20 +0100

    renaming models

--- models/mart/mrt_order_daily_report.sql
+++ models/mart/mrt_order_daily_report.sql
@@ -5,7 +5,7 @@ SELECT DATE_TRUNC(order_created_at, DAY) AS reporting_date,
     AVG(total_distinct_items) AS average_total_distinct_items,
     SUM(sum_feedback_score) / SUM(total_feedbacks) AS average_feedback_score,
     AVG(total_order_amount) AS average_total_order_amount
-FROM {{ ref('int_sales_database__orders') }} AS orders
+FROM {{ ref('int_sales_db__orders') }} AS orders
 LEFT JOIN {{ ref('stg_google_sheets__account_manager_region_mapping') }} as mapping ON orders.user_state = mapping.state
 GROUP BY report_date,
     account_manager,
